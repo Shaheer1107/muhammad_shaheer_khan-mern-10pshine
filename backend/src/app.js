86bf2dbfe6a4401f8fc56992ed6e1de75ecb919d
Shaheer1107 <m.shaheerkhan280@gmail.com>
2025-09-29 23:48:27 +0500

    Finalizing auth api

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,3 @@
-console.log("🟢 src/app.js is running");
 import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
@@ -11,11 +10,6 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-console.log("➡️ Mounting /api/auth routes");
 app.use("/api/auth", authRoutes);
-
-app.get("/ping", (req, res) => {
-  res.json({ message: "pong" });
-});
 
 export default app;
